1b3f9e6f294d8d4d5f99f501c9f411c86520174f
Paweł Konik <konius85@gmail.com>
2017-11-18 17:21:52 +0100

    Initalize components

---
+++ index.js
@@ -0,0 +1,85 @@
+var Counter = React.createClass({
+
+    getInitialState: function() {
+        return {
+            counter: 0
+        };
+    },
+
+    increment: function() {
+        this.setState({
+            counter: this.state.counter + 1
+        });
+    },
+    getDefaultProps:function(){
+    	console.log('Load data from database')
+    },
+
+    componentDidMount: function(){
+    	console.log('Set Jquery Action');
+    },
+	
+    checkConter: function () {
+		if(this.state.counter <= 0 ){
+			return this.state.counter + 10
+		}else{
+			return this.state.counter - 1
+		}
+    },
+
+    decrement: function () {
+    	this.setState({
+    		counter: this.checkConter()
+    	});
+    },
+
+    render: function() {
+        return React.createElement('div', {},
+            React.createElement('span', {}, this.state.counter),
+            React.createElement('button', {onClick: this.decrement}, '-'),
+            React.createElement('button', {onClick: this.increment}, '+')
+        );
+    }
+
+});
+
+var counters = [
+  {
+    id: 1,
+    firstName: 'Jan',
+    lastName: 'Nowak',
+    email: 'jan.nowak@example.com',
+  },
+  {
+    id: 2,
+    firstName: 'Adam',
+    lastName: 'Kowalski',
+    email: 'adam.kowalski@example.com',
+  },
+  {
+    id: 3,
+    firstName: 'Zbigniew',
+    lastName: 'Koziol',
+    email: 'zbigniew.koziol@example.com',
+  }
+];
+
+
+var Counters = React.createClass({
+  propTypes: {
+    items: React.PropTypes.array.isRequired,
+  },
+
+  render: function() {
+    var counters = this.props.items.map(function(counter) {
+        return React.createElement(Counter, {item: counter, key: counter.id});
+    });
+
+    return (
+      React.createElement('ul', {className: 'counters'}, counters)
+    );
+  }
+});
+
+var element = React.createElement(Counters, {items: counters});
+ReactDOM.render(element, document.getElementById('app'));
