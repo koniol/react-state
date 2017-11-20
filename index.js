a32a3170460e50287d7710b371c7a809f9b26e0a
Paweł Konik <konius85@gmail.com>
2017-11-20 19:19:30 +0100

    Remove obsolete data

--- index.js
+++ index.js
@@ -46,21 +46,12 @@ var Counter = React.createClass({
 var counters = [
   {
     id: 1,
-    firstName: 'Jan',
-    lastName: 'Nowak',
-    email: 'jan.nowak@example.com',
   },
   {
     id: 2,
-    firstName: 'Adam',
-    lastName: 'Kowalski',
-    email: 'adam.kowalski@example.com',
   },
   {
     id: 3,
-    firstName: 'Zbigniew',
-    lastName: 'Koziol',
-    email: 'zbigniew.koziol@example.com',
   }
 ];
 
